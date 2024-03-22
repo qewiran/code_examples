700e253b1dafff7536471dfa608623d6d8884367
qewiran <baldhill228@gmail.com>
2024-03-22 14:42:13 +0300

    add: comments and clang-format

--- my_dummy_device/my_dummy_device.c
+++ my_dummy_device/my_dummy_device.c
@@ -1,62 +1,72 @@
-#include <linux/module.h>
-#include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/module.h>
 
 int major = 1;
 int minor = 0;
 int dev_count = 1;
 dev_t device_number;
 
-struct cdev dummy_cdev;
+struct cdev dummy_cdev; /* представление символьного устройства в ядре*/
 
-struct file_operations dummy_fops = {
-	.owner = THIS_MODULE
-};
+struct file_operations
+    dummy_fops; /*мой драйвер ничего не делает, поэтому
+                   объект стр-ры file_operations не инициализируется */
 
 struct class* class_dummy;
 struct device* device_dummy;
 
-static int __init dummy_init(void){
-	int rv, err;
-	rv = alloc_chrdev_region(&device_number, minor, dev_count, "my_dummy_device_num");
-	if (rv){
-		printk(KERN_WARNING "my_dummy_device: number cannot be allocated! error: %d\n",rv);
-		return rv;
-	}
-	else{
+static int __init dummy_init(void) {
+  int rv, err;
 
-		printk(KERN_INFO "my_dummy_device: device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
-	}
+  /* динамическое выделение номера символьного устройства */
+  rv = alloc_chrdev_region(&device_number, minor, dev_count,
+                           "my_dummy_device_num");
+  if (rv) {
+    printk(KERN_WARNING
+           "my_dummy_device: number cannot be allocated! error: %d\n",
+           rv);
+    return rv;
+  } else {
+    printk(KERN_INFO "my_dummy_device: device number <major>:<minor> = %d:%d\n",
+           MAJOR(device_number), MINOR(device_number));
+  }
 
-	cdev_init(&dummy_cdev, &dummy_fops);
-	
-	dummy_cdev.owner = THIS_MODULE;
-	err = cdev_add(&dummy_cdev,device_number, dev_count);
+  /* инициализация объекта структуры cdev объектом file_operations  */
+  cdev_init(&dummy_cdev, &dummy_fops);
 
-	if (err)
-	{
-		printk(KERN_WARNING "my_dummy_device: cdev add error: %d!\n",err);
-		return err;
-	}
-	class_dummy = class_create(THIS_MODULE,"class_dummy");
-	
-	device_dummy = device_create(class_dummy, NULL, device_number, NULL, "my_dummy_device");
-	
-	printk(KERN_INFO "my_dummy_device: module loaded!\n");
-	return 0;
+  dummy_cdev.owner =
+      THIS_MODULE; /* чтобы драйвер не выгружался во время работы*/
+
+  /* регистрация символьного устройства с полученным номером в ядре*/
+  err = cdev_add(&dummy_cdev, device_number, dev_count);
+
+  if (err) {
+    printk(KERN_WARNING "my_dummy_device: cdev add error: %d!\n", err);
+    return err;
+  }
+
+  /* создаем класс — директорию в /sys/class/ */
+  class_dummy = class_create(THIS_MODULE, "class_dummy");
+
+  /* заносим информацию об устройстве в этот класс */
+  device_dummy =
+      device_create(class_dummy, NULL, device_number, NULL, "my_dummy_device");
+
+  printk(KERN_INFO "my_dummy_device: module loaded!\n");
+  return 0;
 }
 
-static void __exit dummy_exit(void){
-	device_destroy(class_dummy, device_number);
-	class_destroy(class_dummy);
-	cdev_del(&dummy_cdev);
-	unregister_chrdev_region(device_number,dev_count);
-	printk(KERN_INFO "my_dummy_device: module is unloaded!\n");
+static void __exit dummy_exit(void) {
+  /* вызываем cleanup функции для объектов, созданных при загрузке драйвера */
+  device_destroy(class_dummy, device_number);
+  class_destroy(class_dummy);
+  cdev_del(&dummy_cdev);
+  unregister_chrdev_region(device_number, dev_count);
+  printk(KERN_INFO "my_dummy_device: module is unloaded!\n");
 }
 
 module_init(dummy_init);
 module_exit(dummy_exit);
 
 MODULE_LICENSE("GPL");
-
-
